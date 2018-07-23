38f4c41f1c00330e86c32aeb592ea000ca215b26
sa.as <eng.sa.as@gmail.com>
2018-07-23 16:44:42 +0200

    show random list of movies
    without scaffolding write model and controller and view

---
+++ VideoRentalApp/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoRentalApp.Models;
+
+namespace VideoRentalApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            //var movie = new Movie() { Name = "sherk" };
+            //var movie1 = new List<Movie>();
+            List<Movie> movie2 = new List<Movie>() {
+                new Movie{Name="a123"},new Movie{Name="c123"},new Movie{Name="123"}
+                ,new Movie{Name="f123"},
+                new Movie{Name="fr123"},
+                new Movie{Name="jh123"}
+              
+            };
+
+            
+                return View(movie2);
+            
+
+            
+        }
+    }
+}
