56d096cd038d299e47787b3d7d934800905fc27e
sa.as <eng.sa.as@gmail.com>
2018-07-23 18:55:57 +0200

    Add Bootstrap templete
    use Lumen template from
    www.Bootswatch.com

--- VideoRentalApp/App_Start/BundleConfig.cs
+++ VideoRentalApp/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace VideoRentalApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
