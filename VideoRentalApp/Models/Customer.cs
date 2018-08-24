0eb3fd07d546ee7c23ff0dfc7b255ff0b11006ef
sa.as <eng.sa.as@gmail.com>
2018-08-24 12:24:06 +0200

    Override conventions
    By annotation change field attributes

--- VideoRentalApp/Models/Customer.cs
+++ VideoRentalApp/Models/Customer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace VideoRentalApp.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public string Family { get; set; }
         public int Birthday { get; set; }
